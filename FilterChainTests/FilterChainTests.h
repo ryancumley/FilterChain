92542d76f12aeaeb5cd82500da28bbbd4606a58d
RyanCumley <Ryan@RyanCumley.com>
2013-09-06 15:36:48 -0600

    Initial Commit

---
+++ FilterChainTests/FilterChainTests.h
@@ -0,0 +1,13 @@
+//
+//  FilterChainTests.h
+//  FilterChainTests
+//
+//  Created by Ryan Cumley on 9/6/13.
+//  Copyright (c) 2013 Ryan Cumley. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FilterChainTests : SenTestCase
+
+@end
---
+++ FilterChainTests/FilterChainTests.m
@@ -0,0 +1,32 @@
+//
+//  FilterChainTests.m
+//  FilterChainTests
+//
+//  Created by Ryan Cumley on 9/6/13.
+//  Copyright (c) 2013 Ryan Cumley. All rights reserved.
+//
+
+#import "FilterChainTests.h"
+
+@implementation FilterChainTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FilterChainTests");
+}
+
+@end
